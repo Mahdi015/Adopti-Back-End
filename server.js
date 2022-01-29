f85e53be2669389feea8d353aae220691a376f4d
Mahdi <ferianimahdi93@gmail.com>
2022-01-29 19:35:42 +0100

    first commmit

--- server.js
+++ server.js
@@ -1,40 +1,37 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const {readdirSync} = require('fs')
-require('dotenv').config()
-
-
-
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { readdirSync } = require("fs");
+require("dotenv").config();
 
 //app
-const app = express()
+const app = express();
 
 //db
-mongoose.connect(process.env.DATABASE,{
+mongoose
+  .connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify:false,
-    useUnifiedTopology: true ,
-})
-.then(() => console.log(`DB CONNECTED`))
-.catch(err=>console.log(`DB CONNECTION ERROR`,err))
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log(`DB CONNECTED`))
+  .catch((err) => console.log(`DB CONNECTION ERROR`, err));
 
-//middlewares   
+//middlewares
 app.use(morgan("dev"));
-app.use(bodyParser.json({limit:'2mb'}));
+app.use(bodyParser.json({ limit: "2mb" }));
 app.use(cors());
 
 // routes middlewares
 
-readdirSync('./routes').map((r)=>app.use("/api", require('./routes/'+r)))
-
+readdirSync("./routes").map((r) => app.use("/api", require("./routes/" + r)));
 
 //route
 
 //port
 const port = process.env.PORT || 8000;
 
-app.listen(port,()=>console.log(`Server is Running on Port ${port}`));
+app.listen(port, () => console.log(`Server is Running on Port ${port}`));
